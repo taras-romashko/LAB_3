91d191ddc66a9c0f8a1f4ad9f37911ae0eefd340
Taras Romashko <romashko_taras@comp-sc.if.ua>
2020-12-05 17:12:07 +0200

    lab #3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-basics';
+  result = '';
+
+  onSubmit(form: NgForm) {
+    const products = form.value.products;
+    const productList = products.split(',');
+    const filteredList = productList.filter(
+      function(product) {
+        return product.trim();
+      }
+    );
+
+    if (filteredList.length <= 3) this.result = "Welcome!";
+    if (filteredList.length > 3) this.result = "It is too much!";
+    if (filteredList.length === 0) this.result = "Enter data"; 
+  }
 }
+
